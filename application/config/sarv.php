a386a55a2a1a0858b5c62a9a75faede817ac34b3
mehpal <mehpal@mobileappz.in>
2016-04-28 14:32:40 +0530

    mehpal

--- application/config/sarv.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!defined('BASEPATH'))
-    exit('No direct script access allowed');
-
-$config['username'] = 'u12765';
-$config['msg_token'] = 'Ij3aEZ';
-$config['sender_id'] = 'Mysarv';
-
--- application/language/english/message_lang.php
+++
@@ -1,6 +0,0 @@
-<?php
-$lang["msg_first_name"] = "First Name";
-$lang["msg_last_name"] = "Last Name";
-$lang["msg_dob"] = "Date of Birth";
-$lang["msg_address"] = "Address";
-?>
