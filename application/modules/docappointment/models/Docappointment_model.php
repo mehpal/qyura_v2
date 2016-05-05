074de4b51f01d10a67f2a77f88abdfb184862ef3
shoaibquraishi <shoaibquraishi1@gmail.com>
2016-05-05 19:28:09 +0530

    shoaibquraishi

--- application/modules/docappointment/models/Docappointment_model.php
+++ application/modules/docappointment/models/Docappointment_model.php
@@ -352,28 +352,13 @@ CASE
             $where['qyura_specialities.specialities_id'] = $specialityid;
         }
 
-        $this->db->select('qyura_doctors.doctors_roll,qyura_doctors.doctors_mobile,qyura_doctors.doctors_id as id,qyura_doctors.doctors_userId as userId, CONCAT(qyura_doctors.doctors_fName, " ",  qyura_doctors.doctors_lName) AS name, qyura_doctors.doctors_img imUrl, qyura_doctors.doctors_consultaionFee as consFee, Group_concat(DISTINCT qyura_specialities.specialities_name) as specialityName, Group_concat(DISTINCT qyura_degree.degree_SName) as degree, qyura_doctors.doctors_lat as lat, qyura_doctors.doctors_long as long,qyura_doctors.doctors_27Src as isEmergency, ( FROM_UNIXTIME(qyura_professionalExp.professionalExp_end,"%Y") - FROM_UNIXTIME(qyura_professionalExp.professionalExp_start,"%Y"))  AS exp ,(
-CASE 
- WHEN (reviews_rating is not null AND qyura_ratings.rating is not null) 
- THEN
-      ROUND( (AVG(reviews_rating+qyura_ratings.rating))/2, 1)
- WHEN (reviews_rating is not null) 
- THEN 
-      ROUND( (AVG(reviews_rating)), 1)
- WHEN (qyura_ratings.rating is not null) 
- THEN
-      ROUND( (AVG(qyura_ratings.rating)), 1)
- END)
- AS `rating` ')
+        $this->db->select('qyura_doctors.doctors_roll,qyura_doctors.doctors_mobile,qyura_doctors.doctors_id as id,qyura_doctors.doctors_userId as userId, CONCAT(qyura_doctors.doctors_fName, " ",  qyura_doctors.doctors_lName) AS name, qyura_doctors.doctors_img imUrl, qyura_doctors.doctors_consultaionFee as consFee, Group_concat(DISTINCT qyura_specialities.specialities_name) as specialityName, Group_concat(DISTINCT qyura_degree.degree_SName) as degree, qyura_doctors.doctors_lat as lat, qyura_doctors.doctors_long as long,qyura_doctors.doctors_27Src as isEmergency')
                 ->from('qyura_doctors')
                 ->join('qyura_usersRoles', 'qyura_usersRoles.usersRoles_userId=qyura_doctors.doctors_userId', 'left')
                 ->join('qyura_doctorAcademic', 'qyura_doctorAcademic.doctorAcademic_doctorsId=qyura_doctors.doctors_id', 'left')
-                ->join('qyura_professionalExp', 'qyura_professionalExp.professionalExp_usersId=qyura_doctors.doctors_id', 'left')
                 ->join('qyura_degree', 'qyura_doctorAcademic.doctorAcademic_degreeId=qyura_degree.degree_id', 'left')
                 ->join('qyura_doctorSpecialities', 'qyura_doctorSpecialities.doctorSpecialities_doctorsId = qyura_doctors.doctors_id', 'left')
                 ->join('qyura_specialities', 'qyura_specialities.specialities_id = qyura_doctorSpecialities.doctorSpecialities_specialitiesId', 'left')
-                ->join('qyura_reviews', 'qyura_reviews.reviews_relateId=qyura_doctors.doctors_userId', 'left')
-                ->join('qyura_ratings', 'qyura_ratings.rating_relateId=qyura_doctors.doctors_userId', 'left')
                 ->where($where)
                 
 //                ->order_by('distance', 'ASC')
