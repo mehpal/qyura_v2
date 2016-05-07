95292d9885193eeed23eb604e03e580e2500a18e
shoaibquraishi <shoaibquraishi1@gmail.com>
2016-05-07 16:55:13 +0530

    shoaibquraishi

--- application/modules/docappointment/views/addAppScript.php
+++ application/modules/docappointment/views/addAppScript.php
@@ -4,7 +4,171 @@
 <script src="<?php echo base_url(); ?>assets/vendor/select2/select2.min.js" type="text/javascript"></script>
 
 
-<script>
+<script type="text/javascript">
+
+
+    var formValid = $("#setData");
+
+    var url = "<?php echo base_url(); ?>";
+    formValid.validate({
+        rules: {
+            input1: {
+                required: true
+            },
+            input2: {
+                required: true
+            },
+            input4: {
+                required: true
+            },
+            input3: {
+                required: true
+            },
+            input24: {
+                required: true,
+                remote: {
+                    url: url + 'index.php/docappointment/check_timeslot',
+                    type: "post",
+                    data: {
+                        timeslot_id: function () {var timeSlot = $("#timeSlot").val().split(',');return timeSlot[0];},
+                        final_timing: function () {return $("#timepicker4").val();},
+                    }
+                }
+            },
+            input8: {
+                required: true
+            },
+            input9: {
+                    required: true,
+                    email: true,
+                },
+            input10: {
+                required: true,
+                number: true,
+                minlength: 10,
+                maxlength: 10
+            },
+            input12: {
+                required: true
+            },
+            input26: {
+                required: true
+            },
+            input27: {
+                required: true
+            },
+            input13: {
+                required: true
+            },
+            input14: {
+                required: true
+            },
+            input15: {
+                required: true
+            },
+            input16: {
+                required: true,
+                number: true,
+                minlength: 6,
+                maxlength: 6
+            },
+            input17: {
+                required: true
+            },
+            input18: {
+                required: true,
+                numberdecimalonly: true
+            },
+            input19: {
+                required: true,
+                numberdecimalonly: true
+            },
+            input20: {
+                required: true,
+                numberdecimalonly: true
+            },
+            input21: {
+                required: true,
+                numberdecimalonly: true
+            },
+            input22: {
+                required: true
+            },
+        },
+        messages: {
+            input1: {
+                required: "Please select City.",
+            },
+            input2: {
+                required: "Please select Speciality."
+            },
+            input4: {
+                required: "Please select Date."
+            },
+            input3: {
+                required: "Please select Assign Doctor."
+            },
+            input24: {
+                required: "Please select Time Slot.",
+                remote: "Please select correct time slot."
+            },
+            input8: {
+                required: "Please enter Patient Remarks."
+            },
+            input9:{
+                 required: "Please enter email",
+            }, 
+            input10: {
+                required: "Please enter Patient Mobile Number."
+            },
+            input12: {
+                required: "Please enter Patient Name."
+            },
+            input26: {
+                required: "Please select DOB."
+            },
+            input27: {
+                required: "Please select Gender."
+            },
+            input13: {
+                required: "Please select Country."
+            },
+            input14: {
+                required: "Please select State."
+            },
+            input15: {
+                required: "Please select City."
+            },
+            input16: {
+                required: "Please enter Zip Code."
+            },
+            input17: {
+                required: "Please enter Address."
+            },
+            input18: {
+                required: "Please enter Consulation Fee."
+            },
+            input19: {
+                required: "Please enter Other Fee."
+            },
+            input20: {
+                required: "Please enter Tax."
+            },
+            input21: {
+                required: "Please enter Total Payable Amount."
+            },
+            input22: {
+                required: "Please select Payment Status."
+            },
+            input23: {
+                required: "Please select Payment Mode."
+            }
+        }
+    });
+
+
+
+
     function fetchCity(stateId, cityId, id) {
         $.ajax({
             url: '<?php echo site_url('docappointment/fetchCity'); ?>',
@@ -36,9 +200,9 @@
     function getMI(option) {
         var city_id = $("#appointment_city").val();
         var appointment_type = $("#centerType").val();
-        
+
         var url = '<?php echo site_url(); ?>/docappointment/getMI';
-        if (typeof city_id == 'string' && typeof appointment_type == 'string'){
+        if (typeof city_id == 'string' && typeof appointment_type == 'string') {
             $.ajax({
                 url: url,
                 async: false,
@@ -53,9 +217,9 @@
                     $('#mi_centre').selectpicker('refresh');
                     $('#timeSlot').html('');
                     $('#timeSlot').selectpicker('refresh');
-                    $('#input5').prop('selectedIndex','');
+                    $('#input5').prop('selectedIndex', '');
                     $('#input5').selectpicker('refresh');
-                    $('#speciallity').prop('selectedIndex','');
+                    $('#speciallity').prop('selectedIndex', '');
                     $('#speciallity').selectpicker('refresh');
                     $("#mi_centre").removeClass('loadinggif');
                     $('#doctorSection').hide();
@@ -78,9 +242,9 @@
             },
             success: function (data) {
                 $("#patient_email").removeClass('loadinggif');
-                if(data && data != 0){
+                if (data && data != 0) {
                     var data = JSON.parse(data);
-                    if(data.email_status != 1){
+                    if (data.email_status != 1) {
                         console.log(data.mobile);
                         $('#users_mobile').val(data.mobile);
                         $('#stateId').val(data.stateId);
@@ -94,28 +258,28 @@
                         $("#familyDiv").show();
                         $('#zip').val(data.pin);
                         $('#date-4').val(data.dob);
-                        $('#input27').prop('selectedIndex',data.gender);
+                        $('#input27').prop('selectedIndex', data.gender);
                         $('#input27').selectpicker('refresh');
                         $('#user_id').val(data.user_id);
-                    }else{
+                    } else {
                         $('#user_id').val(data.id);
                         $('#email_status').val(data.email_status);
                     }
-                }else{
+                } else {
                     $('#users_mobile').val('');
                     $('#cityId').html('');
                     $('#cityId').selectpicker('refresh');
-                    $('#stateId').prop('selectedIndex','');
+                    $('#stateId').prop('selectedIndex', '');
                     $('#stateId').selectpicker('refresh');
                     $('#users_username').val('');
                     $('#address').val('');
                     $('#unqId').val('');
                     $('#zip').val('');
                     $('#date-4').val('');
-                    $('#input27').prop('selectedIndex','');
+                    $('#input27').prop('selectedIndex', '');
                     $('#input27').selectpicker('refresh');
                     $('#user_id').val('');
-                    
+
                     $('#input25').html('');
                     $('#input25').selectpicker('refresh');
                     $('#familyDiv').hide();
@@ -125,18 +289,18 @@
             }
         });
     }
-    
-    function getTimeSlot(){
-        
+
+    function getTimeSlot() {
+
         var doc_id = $("#input3").val();
         var date = $("#date-3").val();
         var url = '<?php echo site_url(); ?>/docappointment/appoint_timeSlot';
-        if (typeof doc_id == 'string' && typeof date == 'string'){
+        if (typeof doc_id == 'string' && typeof date == 'string') {
             $.ajax({
                 url: url,
                 async: false,
                 type: 'POST',
-                data: {'doc_id': doc_id,'date': date},
+                data: {'doc_id': doc_id, 'date': date},
                 beforeSend: function (xhr) {
                     $("#timeSlot").addClass('loadinggif');
                 },
@@ -145,60 +309,61 @@
                     $('#timeSlot').html(data);
                     $('#timeSlot').selectpicker('refresh');
                     $("#timeSlot").removeClass('loadinggif');
-                    $('#input5').prop('selectedIndex','');
+                    $('#input5').prop('selectedIndex', '');
                     $('#input5').selectpicker('refresh');
                 }
             });
         }
     }
-    
-    function findDoc(){
+
+    function findDoc() {
         var city_id = $("#appointment_city").val();
         var special_id = $('#speciallity').val();
         var url = '<?php echo site_url(); ?>/docappointment/find_doctor';
-        if (typeof city_id == 'string' && typeof special_id == 'string' && special_id != ''){
+        if (typeof city_id == 'string' && typeof special_id == 'string' && special_id != '') {
             $.ajax({
                 url: url,
                 async: false,
                 type: 'POST',
-                data: {'city_id': city_id,'special_id':special_id},
-                beforeSend: function (xhr) {},
+                data: {'city_id': city_id, 'special_id': special_id},
+                beforeSend: function (xhr) {
+                },
                 success: function (data) {
                     $('#input3').html(data);
                     $('#input3').selectpicker('refresh');
                     $("#input3").removeClass('loadinggif');
-                    $('#input5').prop('selectedIndex','');
+                    $('#input5').prop('selectedIndex', '');
                     $('#input5').selectpicker('refresh');
                 }
             });
         }
     }
-    
-    function calculateamount(){
-        
+
+    function calculateamount() {
+
         var con_fee = parseInt($('#input18').val());
         var oth_fee = parseInt($('#input19').val());
         var tax = parseInt($('#input20').val());
         var amount = con_fee + oth_fee;
-        
-        var tax_amount = (amount/parseInt(100))*tax;
-        var total_amount = amount + tax_amount; 
-        
-        if(total_amount){
+
+        var tax_amount = (amount / parseInt(100)) * tax;
+        var total_amount = amount + tax_amount;
+
+        if (total_amount) {
             $("#input21").val(total_amount);
-        }else if(!amount){
+        } else if (!amount) {
             $("#input21").val(con_fee);
-        }else if(amount){
+        } else if (amount) {
             $("#input21").val(amount);
-        }else{
+        } else {
             $("#input21").val('');
         }
     }
-    
- function getMember(obj){
-        if(obj == 1){
+
+    function getMember(obj) {
+        if (obj == 1) {
             var user_id = $("#user_id").val();
-            if(user_id != ''){
+            if (user_id != '') {
                 var url = '<?php echo site_url(); ?>/docappointment/getMember/';
                 $.ajax({
                     url: url,
@@ -216,62 +381,40 @@
                     }
                 });
             }
-        }else{
+        } else {
             $('#familyListDiv').hide();
             $('#input25').html('');
             $('#input25').selectpicker('refresh');
         }
     }
-    
+
     $('#date-3, #date-4').datepicker({autoclose: true});
-    $('.timepicker').timepicker({showMeridian:false});
+    $('.timepicker').timepicker({showMeridian: true});
     $(document).ready(function () {
         $("#setData").submit(function (event) {
+
+            if (!formValid.valid())
+                return false;
+
             event.preventDefault();
             var url = '<?php echo site_url(); ?>/docappointment/addAppointmentSave/';
             var formData = new FormData(this);
-            submitData(url,formData);
+            submitData(url, formData);
         });
     });
     $(".select2").select2({
         width: '100%'
     });
 
-    $(".bs-select").select2({
-        placeholder: "Select a Speciality",
-        //allowClear: true,
-        tags: true
-    });
-    
-    $(document).ready(function (){
-var urls = "<?php echo base_url() ?>";
-$("#submitForm").validate({
-    rules: {
-    input1: {
-        required: true
-        },
-    input2: {
-        required: true
-        },
-    input4: {
-        required: true
-        },
-    },
-    messages: {
-            input1: {
-                required: "Please enter scientific name!",
-            },
-            input2: {
-                required: "Please enter general name!"
-            },
-            input3: {
-                required: "Please upload an image!"
-            }
-        }
-    });
-});
-    
-    function check_validaton(){
+//    $(".bs-select").select2({
+//        placeholder: "Select a Speciality",
+//        //allowClear: true,
+//        tags: true
+//    });
+
+
+
+    function check_validaton() {
         var url = '<?php echo site_url(); ?>/docappointment/check_timeslot/';
         var final_timing = $("#timepicker4").val();
         var timeslot_id = $("#timeSlot").val();
@@ -280,24 +423,16 @@ $("#submitForm").validate({
             url: url,
             async: false,
             type: 'POST',
-            data: {'timeslot_id' : timeslot_id,'final_timing': final_timing},
-            beforeSend: function (xhr) {
-                $("#input25").addClass('loadinggif');
-            },
+            data: {'timeslot_id': timeslot_id, 'final_timing': final_timing},
             success: function (data) {
-                if(data == 1){
+                if (data == 1) {
                     $("#err_timepicker4").hide();
                     resultAjax = 0;
-                }else{
+                } else {
                     $("#err_timepicker4").show();
                     resultAjax++;
                 }
             }
         });
-        if(resultAjax == 0){
-            return true;
-        }else{
-            return false;
-        }
     }
 </script>
