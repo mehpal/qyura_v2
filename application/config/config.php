2363ba7785e78b50d246689109d8a934d77a74c1
mehpal <mehpal@mobileappz.in>
2016-05-09 11:16:19 +0530

    mehpal update

--- application/config/config.php
+++ application/config/config.php
@@ -18,11 +18,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://ec2-54-218-61-103.us-west-2.compute.amazonaws.com/qyura_v2/';
+//$config['base_url'] = 'http://ec2-54-218-61-103.us-west-2.compute.amazonaws.com/qyura_v2/';
 //$config['base_url'] = "http://".$_SERVER['HTTP_HOST'];
 
 
-// $config['base_url'] = 'http://localhost/qyura_v2';
+ $config['base_url'] = 'http://localhost/qyura';
  
 /*
 |--------------------------------------------------------------------------
