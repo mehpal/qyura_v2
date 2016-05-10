f17f15f6af31c392fa212d683ddfd7c2bc7d589e
pawansen <pawansen.mobileappz@gmail.com>
2016-05-10 10:36:38 +0530

    added dashboard pawan

--- application/modules/sadashboard/controllers/Sadashboard.php
+++ application/modules/sadashboard/controllers/Sadashboard.php
@@ -1,20 +1,27 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class SaDashboard extends MY_Controller {
-    
-public function __construct() {
+
+    public function __construct() {
         parent:: __construct();
-        $this->load->model(array("Common_model"));
-        
-        
+        $this->load->model(array("common_model","dashboard_model"));
     }
-	
-	public function index()
-	{
-            $this->Common_model->mypermission("7");
-	    $data['title'] = 'SuperAdmin Dashboard';
-            $this->load->super_admin_template('Superadmin_dashboard');
-	}
-       
+
+    public function index() {
+        $this->common_model->mypermission("7");
+
+        $data['MI'] = $this->dashboard_model->getMiCount();
+        $data['Doctor'] = $this->dashboard_model->getDoctorCount();
+        $data['User'] = $this->dashboard_model->getUserCount();
+        $data['MiList'] = $this->dashboard_model->getMiList();
+        
+        
+        dump($data['MiList']);
+        exit();
+        $data['title'] = 'SuperAdmin Dashboard';
+        $this->load->super_admin_template('Superadmin_dashboard', $data, 'dashboardScript');
+    }
+
 }
