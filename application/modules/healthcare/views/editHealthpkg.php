044267a19cd15f8a0bd364c7b646ec86e4e6457b
Hemu <hp.mobileappz@gmail.com>
2016-05-10 12:17:30 +0530

    by hemu

--- application/modules/healthcare/views/editHealthpkg.php
+++ application/modules/healthcare/views/editHealthpkg.php
@@ -171,12 +171,12 @@
 
                                 <article class="form-group m-lr-0">
                                     <label for="cname" class="control-label col-md-4  col-sm-4">Test Includes:</label>
-                                    <div class="col-md-8 col-sm-8">
+                                    <div class="col-md-8 col-sm-8"  id="multiserviceName">
                                         <a href="javascript:void(0)" class="add pull-right" onclick="countserviceName()" ><i class="fa fa-plus-circle fa-2x m-t-5 label-plus"></i></a>
 
                                         <aside class="row clone">
 
-                                            <div class="col-lg-10 col-md-10 col-sm-7 col-xs-10 m-t-xs-10" id="multiserviceName">
+                                            <div class="col-lg-10 col-md-10 col-sm-7 col-xs-10 m-t-xs-10">
                                                 <?php
                                                 if($editHealthcareData[0]->test != ''){
 
@@ -190,9 +190,12 @@
                                                 ?>
                                                 <input type="text" class="form-control" name="testIncluded[]" id="hospitalServices_serviceName1" maxlength="200" value="<?php echo $value; ?>" />
                                                 <?php }else{ ?>
-                                               <div> <a class="remove" href="#" onclick="$(this).parent().slideUp(function(){ $(this).remove() }); return false"> <i class="fa fa-minus-circle fa-2x m-t-5 label-plus"></i></a>
+                                               
+                                                   <aside class="col-sm-2 pull-right"><a class="remove" href="#" onclick="$(this).parent().slideUp(function(){ $(this).remove() }); return false"> <i class="fa fa-minus-circle fa-2x m-t-5 label-plus"></i></a>
+                                                   </aside>
+                                                   <aside class="col-sm-10">
                                                     <input type="text" class="form-control" name="testIncluded[]" id="hospitalServices_serviceName1"maxlength="200" value="<?php echo $value; ?>" />
-                                                </div>
+                                               </aside>
                                              <?php   }
                                             $i++;       }
                                                    
