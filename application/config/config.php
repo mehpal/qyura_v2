207b8f6e86be594b6b70dfcd996eecc5fedf4f8f
mehpal <mehpal@mobileappz.in>
2016-05-10 16:55:46 +0530

    mehpal

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 
- $config['base_url'] = 'http://localhost/qyura';
+ $config['base_url'] = 'http://localhost/qweb/qyura';
 
 
  
