8b63728edc212214b348e0eb114692fff034a9cc
Shilky <shilky@mobileappz.in>
2016-05-14 14:01:05 +0000

    Update

--- application/logs/log-2016-05-14.php
+++ application/logs/log-2016-05-14.php
@@ -5,3 +5,10 @@ FROM `qyura_hospitalTimeSlot`
 WHERE `qyura_hospitalTimeSlot`.`hospitalTimeSlot_deleted` =0
 AND `qyura_hospitalTimeSlot`.`hospitalTimeSlot_hospitalId` = '5'
 ERROR - 2016-05-14 12:32:19 --> Unable to save the image. Please make sure the image and file directory are writable.
+ERROR - 2016-05-14 13:11:56 --> Unable to save the image. Please make sure the image and file directory are writable.
+ERROR - 2016-05-14 13:37:13 --> Severity: Error --> Call to undefined method Common_model::sendMail() /var/www/html/qyura_v2/application/modules/api/controllers/auth/Auth.php 151
+ERROR - 2016-05-14 13:37:45 --> Severity: Error --> Call to undefined method Common_model::sendMail() /var/www/html/qyura_v2/application/modules/api/controllers/auth/Auth.php 151
+ERROR - 2016-05-14 13:41:36 --> 404 Page Not Found: ../modules/api/controllers//index
+ERROR - 2016-05-14 13:41:46 --> 404 Page Not Found: ../modules/api/controllers//index
+ERROR - 2016-05-14 13:44:59 --> 404 Page Not Found: ../modules/api/controllers//index
+ERROR - 2016-05-14 13:46:45 --> 404 Page Not Found: ../modules/api/controllers/auth//index
