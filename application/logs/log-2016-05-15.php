9b3ecb41130ab2b9d336829c86795daf8446ce39
mobileappz114 <hemantrawat.mobileappz@gmail.com>
2016-05-15 19:19:05 +0530

    by hemnat

--- application/logs/log-2016-05-15.php
+++ application/logs/log-2016-05-15.php
@@ -587,3 +587,90 @@ JOIN `qyura_city` `Ct` ON `Ct`.`city_id` = `Spon`.`sponsor_cityId`
 WHERE `Spon`.`sponsor_deleted` =0
 ORDER BY `Spon`.`sponsor_date` DESC, `Spon`.`sponsor_date` DESC, `miname` ASC
  LIMIT 10
+ERROR - 2016-05-15 13:11:24 --> Unable to save the image. Please make sure the image and file directory are writable.
+ERROR - 2016-05-15 13:21:39 --> Query error: Subquery returns more than 1 row - Invalid query: SELECT `Spon`.`sponsor_id`, `healthTips_detail`, `healthTips_image`, `healthTips_amount`, `category_name`, `city_name`, FROM_UNIXTIME(sponsor_date, '%d-%m-%Y') sponsor_date, CASE 
+WHEN (Spon.sponser_userRole = 1 ) THEN (select hospital_name from qyura_hospital hos where hos.hospital_usersId=sponsor_userId)  
+WHEN (Spon.sponser_userRole = 3 ) THEN (select diagnostic_name from qyura_diagnostic diag where diag.diagnostic_usersId=sponsor_userId) 
+WHEN (Spon.sponser_userRole = 4 ) THEN (select concat(doctors_fName, ' ', doctors_lName) from qyura_doctors doc where doc.doctors_userId=sponsor_userId)  end as miname, CASE 
+WHEN (Spon.sponser_userRole = 1 ) THEN 'Hospital'  
+WHEN (Spon.sponser_userRole = 3 ) THEN 'Diagnostic Center' 
+WHEN (Spon.sponser_userRole = 4 ) THEN 'Doctor'  end as mitype
+FROM `qyura_healthTipSponsor` AS `Spon`
+JOIN `qyura_healthTips` AS `HTip` ON `Spon`.`sponsor_tipId`=`HTip`.`healthTips_id`
+JOIN `qyura_healthCategory` `Cat` ON `HTip`.`healthTips_categoryId` = `Cat`.`category_id`
+JOIN `qyura_city` `Ct` ON `Ct`.`city_id` = `Spon`.`sponsor_cityId`
+WHERE `Spon`.`sponsor_deleted` =0
+ORDER BY `Spon`.`sponsor_date` DESC, `Spon`.`sponsor_date` DESC, `miname` ASC
+ LIMIT 10
+ERROR - 2016-05-15 13:21:44 --> Query error: Subquery returns more than 1 row - Invalid query: SELECT `Spon`.`sponsor_id`, `healthTips_detail`, `healthTips_image`, `healthTips_amount`, `category_name`, `city_name`, FROM_UNIXTIME(sponsor_date, '%d-%m-%Y') sponsor_date, CASE 
+WHEN (Spon.sponser_userRole = 1 ) THEN (select hospital_name from qyura_hospital hos where hos.hospital_usersId=sponsor_userId)  
+WHEN (Spon.sponser_userRole = 3 ) THEN (select diagnostic_name from qyura_diagnostic diag where diag.diagnostic_usersId=sponsor_userId) 
+WHEN (Spon.sponser_userRole = 4 ) THEN (select concat(doctors_fName, ' ', doctors_lName) from qyura_doctors doc where doc.doctors_userId=sponsor_userId)  end as miname, CASE 
+WHEN (Spon.sponser_userRole = 1 ) THEN 'Hospital'  
+WHEN (Spon.sponser_userRole = 3 ) THEN 'Diagnostic Center' 
+WHEN (Spon.sponser_userRole = 4 ) THEN 'Doctor'  end as mitype
+FROM `qyura_healthTipSponsor` AS `Spon`
+JOIN `qyura_healthTips` AS `HTip` ON `Spon`.`sponsor_tipId`=`HTip`.`healthTips_id`
+JOIN `qyura_healthCategory` `Cat` ON `HTip`.`healthTips_categoryId` = `Cat`.`category_id`
+JOIN `qyura_city` `Ct` ON `Ct`.`city_id` = `Spon`.`sponsor_cityId`
+WHERE `Spon`.`sponsor_deleted` =0
+ORDER BY `Spon`.`sponsor_date` DESC, `Spon`.`sponsor_date` DESC, `miname` ASC
+ LIMIT 10
+ERROR - 2016-05-15 13:21:51 --> Query error: Subquery returns more than 1 row - Invalid query: SELECT `Spon`.`sponsor_id`, `healthTips_detail`, `healthTips_image`, `healthTips_amount`, `category_name`, `city_name`, FROM_UNIXTIME(sponsor_date, '%d-%m-%Y') sponsor_date, CASE 
+WHEN (Spon.sponser_userRole = 1 ) THEN (select hospital_name from qyura_hospital hos where hos.hospital_usersId=sponsor_userId group by hos.hospital_id)  
+WHEN (Spon.sponser_userRole = 3 ) THEN (select diagnostic_name from qyura_diagnostic diag where diag.diagnostic_usersId=sponsor_userId group by diag.diagnostic_id) 
+WHEN (Spon.sponser_userRole = 4 ) THEN (select concat(doctors_fName, ' ', doctors_lName) from qyura_doctors doc where doc.doctors_userId=sponsor_userId group by doc.doctors_id)  end as miname, CASE 
+WHEN (Spon.sponser_userRole = 1 ) THEN 'Hospital'  
+WHEN (Spon.sponser_userRole = 3 ) THEN 'Diagnostic Center' 
+WHEN (Spon.sponser_userRole = 4 ) THEN 'Doctor'  end as mitype
+FROM `qyura_healthTipSponsor` AS `Spon`
+JOIN `qyura_healthTips` AS `HTip` ON `Spon`.`sponsor_tipId`=`HTip`.`healthTips_id`
+JOIN `qyura_healthCategory` `Cat` ON `HTip`.`healthTips_categoryId` = `Cat`.`category_id`
+JOIN `qyura_city` `Ct` ON `Ct`.`city_id` = `Spon`.`sponsor_cityId`
+WHERE `Spon`.`sponsor_deleted` =0
+ORDER BY `Spon`.`sponsor_date` DESC, `Spon`.`sponsor_date` DESC, `miname` ASC
+ LIMIT 10
+ERROR - 2016-05-15 13:25:07 --> Query error: Subquery returns more than 1 row - Invalid query: SELECT `Spon`.`sponsor_id`, `healthTips_detail`, `healthTips_image`, `healthTips_amount`, `category_name`, `city_name`, FROM_UNIXTIME(sponsor_date, '%d-%m-%Y') sponsor_date, CASE 
+WHEN (Spon.sponser_userRole = 1 ) THEN (select hospital_name from qyura_hospital hos where hos.hospital_usersId=sponsor_userId group by hos.hospital_id)  
+WHEN (Spon.sponser_userRole = 3 ) THEN (select diagnostic_name from qyura_diagnostic diag where diag.diagnostic_usersId=sponsor_userId group by diag.diagnostic_id) 
+WHEN (Spon.sponser_userRole = 4 ) THEN (select concat(doctors_fName, ' ', doctors_lName) from qyura_doctors doc where doc.doctors_userId=sponsor_userId group by doc.doctors_id)  end as miname, CASE 
+WHEN (Spon.sponser_userRole = 1 ) THEN 'Hospital'  
+WHEN (Spon.sponser_userRole = 3 ) THEN 'Diagnostic Center' 
+WHEN (Spon.sponser_userRole = 4 ) THEN 'Doctor'  end as mitype
+FROM `qyura_healthTipSponsor` AS `Spon`
+JOIN `qyura_healthTips` AS `HTip` ON `Spon`.`sponsor_tipId`=`HTip`.`healthTips_id`
+JOIN `qyura_healthCategory` `Cat` ON `HTip`.`healthTips_categoryId` = `Cat`.`category_id`
+JOIN `qyura_city` `Ct` ON `Ct`.`city_id` = `Spon`.`sponsor_cityId`
+WHERE `Spon`.`sponsor_deleted` =0
+ORDER BY `Spon`.`sponsor_date` DESC, `Spon`.`sponsor_date` DESC, `miname` ASC
+ LIMIT 10
+ERROR - 2016-05-15 13:25:15 --> Query error: Subquery returns more than 1 row - Invalid query: SELECT `Spon`.`sponsor_id`, `healthTips_detail`, `healthTips_image`, `healthTips_amount`, `category_name`, `city_name`, FROM_UNIXTIME(sponsor_date, '%d-%m-%Y') sponsor_date, CASE 
+WHEN (Spon.sponser_userRole = 1 ) THEN (select hospital_name from qyura_hospital hos where hos.hospital_usersId=sponsor_userId group by hos.hospital_id)  
+WHEN (Spon.sponser_userRole = 3 ) THEN (select diagnostic_name from qyura_diagnostic diag where diag.diagnostic_usersId=sponsor_userId group by diag.diagnostic_id) 
+WHEN (Spon.sponser_userRole = 4 ) THEN (select concat(doctors_fName, ' ', doctors_lName) from qyura_doctors doc where doc.doctors_userId=sponsor_userId group by doc.doctors_id)  end as miname, CASE 
+WHEN (Spon.sponser_userRole = 1 ) THEN 'Hospital'  
+WHEN (Spon.sponser_userRole = 3 ) THEN 'Diagnostic Center' 
+WHEN (Spon.sponser_userRole = 4 ) THEN 'Doctor'  end as mitype
+FROM `qyura_healthTipSponsor` AS `Spon`
+JOIN `qyura_healthTips` AS `HTip` ON `Spon`.`sponsor_tipId`=`HTip`.`healthTips_id`
+JOIN `qyura_healthCategory` `Cat` ON `HTip`.`healthTips_categoryId` = `Cat`.`category_id`
+JOIN `qyura_city` `Ct` ON `Ct`.`city_id` = `Spon`.`sponsor_cityId`
+WHERE `Spon`.`sponsor_deleted` =0
+ORDER BY `Spon`.`sponsor_date` DESC, `Spon`.`sponsor_date` DESC, `miname` ASC
+ LIMIT 10
+ERROR - 2016-05-15 13:26:07 --> Query error: Subquery returns more than 1 row - Invalid query: SELECT `Spon`.`sponsor_id`, `healthTips_detail`, `healthTips_image`, `healthTips_amount`, `category_name`, `city_name`, FROM_UNIXTIME(sponsor_date, '%d-%m-%Y') sponsor_date, CASE 
+WHEN (Spon.sponser_userRole = 1 ) THEN (select hospital_name from qyura_hospital hos where hos.hospital_usersId=sponsor_userId group by hos.hospital_id)  
+WHEN (Spon.sponser_userRole = 3 ) THEN (select diagnostic_name from qyura_diagnostic diag where diag.diagnostic_usersId=sponsor_userId group by diag.diagnostic_id) 
+WHEN (Spon.sponser_userRole = 4 ) THEN (select concat(doctors_fName, ' ', doctors_lName) from qyura_doctors doc where doc.doctors_userId=sponsor_userId group by doc.doctors_id)  end as miname, CASE 
+WHEN (Spon.sponser_userRole = 1 ) THEN 'Hospital'  
+WHEN (Spon.sponser_userRole = 3 ) THEN 'Diagnostic Center' 
+WHEN (Spon.sponser_userRole = 4 ) THEN 'Doctor'  end as mitype
+FROM `qyura_healthTipSponsor` AS `Spon`
+JOIN `qyura_healthTips` AS `HTip` ON `Spon`.`sponsor_tipId`=`HTip`.`healthTips_id`
+JOIN `qyura_healthCategory` `Cat` ON `HTip`.`healthTips_categoryId` = `Cat`.`category_id`
+JOIN `qyura_city` `Ct` ON `Ct`.`city_id` = `Spon`.`sponsor_cityId`
+WHERE `Spon`.`sponsor_deleted` =0
+ORDER BY `Spon`.`sponsor_date` DESC, `Spon`.`sponsor_date` DESC, `miname` ASC
+ LIMIT 10
+ERROR - 2016-05-15 13:29:04 --> Unable to save the image. Please make sure the image and file directory are writable.
+ERROR - 2016-05-15 13:35:04 --> Unable to save the image. Please make sure the image and file directory are writable.
--- application/modules/api/controllers/HealthTipApi.php
+++ application/modules/api/controllers/HealthTipApi.php
@@ -16,22 +16,26 @@ class HealthTipApi extends MyRest {
 
     function todaytip_post() {
            
-            echo $cityId = isset($_POST['cityId']) ? $this->input->post('cityId') : NULL; 
-//            echo  $date = strtotime(date("Y-m-d")); 
+            $cityId = isset($_POST['cityId']) ? $this->input->post('cityId') : NULL; 
+            $date = strtotime(date("Y-m-d")); 
            
             $sponserTip = NULL;
-            if($cityId != NULL) {
-                echo "Hi";
+            
+            if($cityId != NULL) { 
                 $sponserTip = $this->HealthTip_model->getSponsorTip($cityId,$date);
+//                echo $this->db->last_query();
             }
+            
+//            dump($sponserTip);die();
             
             if(isset($sponserTip) && $sponserTip != NULL){
                 $response['status'] = TRUE;
                 $response["data"] = $sponserTip;
                 $response['msg'] = 'success';
                 $this->response($response, 200);
+                return;
             }else{
-                echo "HERE";
+//                echo "HERE";
                 $response['data'] = $sponserTip = $this->HealthTip_model->getRandomTip($date);
                 
                 if(isset($sponserTip) && $sponserTip != NULL){
