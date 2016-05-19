27be6908a840eb5606f2c748c0c0f05e3a02cc79
Your Name <shilky@mobileappz.in>
2016-05-19 13:31:11 +0530

    Prachi MI Bug

--- application/modules/miappointment/controllers/Miappointment.php
+++ application/modules/miappointment/controllers/Miappointment.php
@@ -12,6 +12,7 @@ class Miappointment extends MY_Controller {
     public function __construct() {
         parent:: __construct();
         $this->load->model('miappointment_model', 'miappointment', 'common_model');
+        $this->common_model->change_appointment_status();
 //        $this->load->library(array('api/ion_auth_api', 'bf_form_validation'));
 //        $this->load->helper(array('url', 'language','common','string'));
 //        $this->bf_bf_form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'api/auth_conf_api'), $this->config->item('error_end_delimiter', 'api/auth_conf_api'));
@@ -26,6 +27,7 @@ class Miappointment extends MY_Controller {
      * @return html
      */
     public function index() {
+      
        $data = array();
        $data['title'] = 'List Mi Appointment';
 //       $msg = $this->load->view('email/signing_up_doctor_tpl', '', true);
